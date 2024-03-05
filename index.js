2064e436e7644b04926fd181960c95e5e8fe0434
vitornoms <vitornoms@gmail.com>
2024-03-05 11:43:49 -0300

    exerciciosprontos

--- index.js
+++ index.js
@@ -3,10 +3,45 @@ console.log("GREMIO NÃO TEM MUNDIAL")
 
 // EXERCÍCIO DE CALCULOS
 
-const num1 = 20;
-const num2 = 654
 
-// SOMA
+// EXERCÍCIO DE SOMA
+const num1 = 20;
+const num2 = 654;
+
+// VARIAVEL SOMA
 let soma = num1 + num2
 
 console.log("O valor da soma é " + soma + " - VAMO INTER");
+
+// EXERCÍCIO DE SUBTRAÇÃO
+
+const num3 = 25;
+const num4 = 400;
+
+// VARIAVEL SUBTRAÇÃO
+
+let subtracao = num3 - num4
+
+console.log("O valor da subtração é " + subtracao);
+
+// EXERCÍCIO DE MULTIPLICAÇÃO
+
+const num5 = 5;
+const num6 = 3;
+
+// VARIAVEL MULTIPLICAÇÃO
+
+let multiplicacao = num5 * num6
+
+console.log("O valor da multiplicacao é " + multiplicacao);
+
+// EXERCÍCIO DE DIVISÃO
+
+const num7 = 3;
+const num8 = 15;
+
+// VARIAVEL DIVISÃO
+
+let divisao = num8 / num7
+
+console.log("O valor da divisão é " + divisao);
