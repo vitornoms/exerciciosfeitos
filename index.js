84dfd575a5cfe08b2efe2b6d7f6d73a4be478ace
vitornoms <vitornoms@gmail.com>
2024-03-05 11:25:41 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,12 @@
+// COMANDO PARA EXIBIR A MENSAGEM NO TERMINAL
+console.log("GREMIO NÃO TEM MUNDIAL")
+
+// EXERCÍCIO DE CALCULOS
+
+const num1 = 20;
+const num2 = 654
+
+// SOMA
+let soma = num1 + num2
+
+console.log("O valor da soma é " + soma + " - VAMO INTER");
